f97e9de64cc22c3190ba930355fc2daf07992852
Kyle Garrison <kgarrison343@gmail.com>
2018-07-11 23:15:12 -0400

    Add skeletal package.

---
+++ bot.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const winston = require('winston');
+const auth = require('./auth.json');
+const { prefix } = require('./config.json');
+
+const myFormat = winston.format.printf(info => {
+  return `${info.timestamp} [${info.level}]: ${info.message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    myFormat),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
+
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands')
+  .filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  //add the command to the list of commands
+  bot.commands.set(command.name, command);
+}
+
+logger.info('initialized. logging in...');
+bot.login(auth.token);
+
+bot.on('ready', function () {
+  logger.info('connected.');
+  logger.info('logged in as: ' + bot.user.username + ' - (' + bot.user.id + ')');
+});
+
+bot.on('message', function(message){
+  if(message.content.substring(0, prefix.length) !== prefix || message.author.bot) {
+    return;
+  }
+  let args = message.content.substring(prefix.length).split(/ +/);
+
+  const commandName = args.shift().toLowerCase();
+
+  if(!bot.commands.has(commandName)) return;
+
+  const command = bot.commands.get(commandName);
+
+  if(command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if(command.usage) {
+      reply += `\nUsage: ${prefix}${command.name} ${command.usage}`;
+    }
+
+    return message.channel.send(reply);
+  }
+
+  try {
+    command.execute(message, args);
+  } catch(error) {
+    logger.error(error);
+    message.reply('There was an error trying to execute that command!');
+  }
+});
