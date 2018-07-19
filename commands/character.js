a20f3c64e2c0ba35bb88d251e4242a8a00e1247c
Kyle Garrison <kgarrison@nremt.org>
2018-07-19 15:54:48 -0400

    Add classes and choosing your class.

--- commands/character.js
+++ commands/character.js
@@ -7,13 +7,22 @@ module.exports = {
 	description: 'Retrieves the user\'s character or creates one if the user does not have one yet.',
 	execute(message) {
 		logger.debug('Executing character (not like that)');
+		let newChar = false;
 		if(!characters.has(message.author.id)){
 			logger.debug('Creating non-existent character for user');
 			characters.set(message.author.id, new Character(message.author.id));
+			newChar = true;
 		}
 		
 		const usersCharacter = characters.get(message.author.id);
-		message.reply(`Here is your character! ${usersCharacter.userId}`)
-		//TODO: create character creation questionnaire.
+		
+		if(!newChar){
+			//TODO: make a embed that displays character information.
+			message.reply(`Here is your character! ${usersCharacter.userId}`)
+		}
+		else {
+			//TODO: Lets put the character creation commands in separate files and write this to 
+			message.reply('Welcome! Please start creating your character with the following commands. I suggest starting with chooseCharacter!');
+		}
 	}
 }
